2607928bbe4b4417fb0b1a205b245bf8a96371a7
Angiie2323 <basila23@go.byuh.edu>
2024-06-18 23:06:06 -1000

    final 1

--- Final 1/scripts/script.js
+++ Final 1/scripts/script.js
@@ -1,81 +1,65 @@
-const gameBoard = document.getElementById('game-board');
-const resetButton = document.getElementById('reset-button');
-let cardsFlipped = [];
-let matchCounter = 0;
-let cardIds = [];
-let cardsChosen = [];
+document.addEventListener('DOMContentLoaded', () => {
+  const cardsArray = [
+      { name: 'cat', img: '/images/cat.png' },
+      { name: 'cat', img: 'images/cat.png' },
+      { name: 'dog', img: 'images/dog.png' },
+      { name: 'dog', img: 'images/dog.png' },
+      { name: 'horse', img: 'images/horse.png' },
+      { name: 'horse', img: 'images/horse.png' },
+      { name: 'rabbit', img: 'images/rabbit.png' },
+      { name: 'rabbit', img: 'images/rabbit.png' }
+  ];
 
-// Create an array of icon classes
-const iconClasses = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
+  cardsArray.sort(() => 0.5 - Math.random());
 
-// Shuffle the icon classes
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-shuffle(iconClasses);
+  const grid = document.querySelector('#memory-game');
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
 
-// Create cards and add event listeners
-function createCards() {
-  for (let i = 0; i < iconClasses.length; i++) {
-    const card = document.createElement('div');
-    card.dataset.iconClass = iconClasses[i];
-    card.addEventListener('click', flipCard);
-    gameBoard.appendChild(card);
-    cardIds.push(card.id);
-  }
-}
-
-// Flip the card and check for matches
-function flipCard() {
-  if (cardsFlipped.length < 2) {
-    cardsFlipped.push(this);
-    this.classList.add('flip');
-    cardsChosen.push(this.dataset.iconClass);
+  function createBoard() {
+      for (let i = 0; i < cardsArray.length; i++) {
+          const card = document.createElement('img');
+          card.setAttribute('src', 'images/blank.png');
+          card.setAttribute('data-id', i);
+          card.addEventListener('click', flipCard);
+          grid.appendChild(card);
+      }
   }
 
-  if (cardsFlipped.length === 2) {
-    setTimeout(checkForMatch, 500);
-  }
-}
-
-// Check for a match and update the game state
-function checkForMatch() {
-  const cards = cardsFlipped;
-  const optionOneId = cards[0].id;
-  const optionTwoId = cards[1].id;
-
-  if (cardsChosen[0] === cardsChosen[1]) {
-    alert('Match!');
-    cards[0].removeEventListener('click', flipCard);
-    cards[1].removeEventListener('click', flipCard);
-    matchCounter++;
-  } else {
-    cards[0].classList.remove('flip');
-    cards[1].classList.remove('flip');
+  function flipCard() {
+      const cardId = this.getAttribute('data-id');
+      cardsChosen.push(cardsArray[cardId].name);
+      cardsChosenId.push(cardId);
+      this.setAttribute('src', cardsArray[cardId].img);
+      if (cardsChosen.length === 2) {
+          setTimeout(checkForMatch, 500);
+      }
   }
 
-  cardsFlipped = [];
-  cardsChosen = [];
+  function checkForMatch() {
+      const cards = document.querySelectorAll('img');
+      const optionOneId = cardsChosenId[0];
+      const optionTwoId = cardsChosenId[1];
 
-  if (matchCounter === iconClasses.length / 2) {
-    alert('Congratulations! You found all the matches!');
-    resetButton.style.display = 'block';
+      if (cardsChosen[0] === cardsChosen[1] && optionOneId !== optionTwoId) {
+          alert('You found a match');
+          cards[optionOneId].setAttribute('src', 'images/white.png');
+          cards[optionTwoId].setAttribute('src', 'images/white.png');
+          cardsWon.push(cardsChosen);
+      } else {
+          cards[optionOneId].setAttribute('src', 'images/blank.png');
+          cards[optionTwoId].setAttribute('src', 'images/blank.png');
+          alert('Sorry, try again');
+      }
+
+      cardsChosen = [];
+      cardsChosenId = [];
+
+      if (cardsWon.length === cardsArray.length/2) {
+          alert('Congratulations! You found them all!');
+      }
   }
-}
 
-// Reset the game
-function resetGame() {
-  matchCounter = 0;
-  cardsFlipped = [];
-  cardsChosen = [];
-  cardIds = [];
-  gameBoard.innerHTML = '';
-  resetButton.style.display = 'none';
-  createCards();
-}
-
-createCards();
-resetButton.style.display = 'none';
+  createBoard();
+});
