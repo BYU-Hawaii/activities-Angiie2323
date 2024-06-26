dc9fcf52805da44f998683d785951aa2798eefb0
Angiie2323 <basila23@go.byuh.edu>
2024-06-25 14:21:21 -1000

    final 1

---
+++ Final 1/scripts/feedback.js
@@ -0,0 +1,7 @@
+
+//Feedback form message
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+    document.getElementById('message').textContent = 'Thank you for submitting the feedback!';
+    document.getElementById('feedbackForm').reset(); // Optionally reset the form fields
+});
